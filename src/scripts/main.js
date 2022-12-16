8b246c4b86a9b702ac600a3a7c2da46c61f58a98
Nadiya Ovsiyenko <nadia.ovsienko1009@gmail.com>
2022-12-16 19:08:08 +0100

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const submit = document.querySelector('#submit');
-
-submit.addEventListener('click', submitClick, false);
-
-function submitClick(event) {
-  event.preventDefault();
-}
